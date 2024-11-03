19f2585f2b1a7b84599a884aa57eb8008035b526
furkangoksu0 <furkangk20@gmail.com>
2024-11-03 19:26:20 +0300

    f

--- src/main/java/org/example/bankaproject/BankaProjectApplication.java
+++ src/main/java/org/example/bankaproject/BankaProjectApplication.java
@@ -2,8 +2,12 @@ package org.example.bankaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "Repository")
+@EntityScan(basePackages = "Entity")
 public class BankaProjectApplication {
 
     public static void main(String[] args) {
